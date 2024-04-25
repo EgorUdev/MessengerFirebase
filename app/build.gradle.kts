57a08970689521f9ccbb9093fc83863981269302
egor.kolbenev <egor.kolbenev@team.wrike.com>
2024-04-25 19:44:33 +0300

    Sign in activity layout added. some test firebase methods added to main activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.androidApplication)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -40,4 +41,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+
 }
--- app/src/main/java/com/example/messengerfirebase/MainActivity.java
+++ app/src/main/java/com/example/messengerfirebase/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.messengerfirebase;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private static final String LOG_TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,112 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Check if user is authorized
+        mAuth = FirebaseAuth.getInstance();
+
+//        FirebaseUser user = mAuth.getCurrentUser();
+//        if (user == null) {
+//            Log.d(LOG_TAG, "Not authorized");
+//        } else {
+//            Log.d(LOG_TAG, "Authorized");
+//        }
+//
+//        //Log out user
+//        mAuth.signOut();
+//
+//
+//        //Check Auth state
+//        mAuth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+//            @Override
+//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+//                FirebaseUser user = mAuth.getCurrentUser();
+//                if (user == null) {
+//                    Log.d(LOG_TAG, "Not authorized");
+//                } else {
+//                    Log.d(LOG_TAG, "Authorized" + user.getUid());
+//                }
+//            }
+//        });
+//
+//        //Log in user
+//        mAuth.signInWithEmailAndPassword("email@email.com", "3111111")
+//                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+//                            @Override
+//                            public void onSuccess(AuthResult authResult) {
+//                                FirebaseUser user = mAuth.getCurrentUser();
+//                                if (user == null) {
+//                                    Log.d(LOG_TAG, "Not authorized");
+//                                } else {
+//                                    Log.d(LOG_TAG, "Authorized");
+//                                }
+//                            }
+//                        }).addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.d(LOG_TAG, Objects.requireNonNull(e.getMessage()));
+//                        Toast.makeText(
+//                                        MainActivity.this,
+//                                        e.getMessage(),
+//                                        Toast.LENGTH_SHORT)
+//                                .show();
+//                    }
+//                });
+
+        //Reset password link send
+        String email = "kesorev+2@gmail.com";
+        mAuth.signInWithEmailAndPassword(email, "000000")
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(LOG_TAG, Objects.requireNonNull(e.getMessage()));
+                        Toast.makeText(
+                                        MainActivity.this,
+                                        e.getMessage(),
+                                        Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+//        mAuth.sendPasswordResetEmail(email)
+//                .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                    @Override
+//                    public void onSuccess(Void unused) {
+//                        Log.d(LOG_TAG, "Mail was sent ou to " + email);
+//                    }
+//                }).addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.d(LOG_TAG, "Mail was NOT sent ou to " + email + " " + e.getMessage());
+//                    }
+//                });
+
+        //Signup new user
+        mAuth.createUserWithEmailAndPassword("email@email.com", "111111")
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        if (user == null) {
+                            Log.d(LOG_TAG, "Not authorized");
+                        } else {
+                            Log.d(LOG_TAG, "Authorized");
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(LOG_TAG, Objects.requireNonNull(e.getMessage()));
+                        Toast.makeText(
+                                        MainActivity.this,
+                                        e.getMessage(),
+                                        Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
     }
 }
